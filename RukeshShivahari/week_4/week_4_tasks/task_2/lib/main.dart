d791ff4f06f2ba58da13e6b7170467b5e9f6c15a
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-03-15 14:39:08 +0545

    feat: Add week 4 task 2

---
+++ RukeshShivahari/week_4/week_4_tasks/task_2/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:task_2/exercises/exercise_one.dart';
+import 'package:task_2/exercises/exercise_two.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ExerciseOne(),
+      // home: ExerciseTwo(),
+    ),
+  );
+}
